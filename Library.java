13a40a1f2a7c871bbab440f76b1e90304474aa9d
MichelleXu00 <mxu32@smith.edu>
2025-04-29 21:14:38 -0400

    attempt 4

--- Library.java
+++ Library.java
@@ -8,6 +8,7 @@ public class Library extends Building implements LibraryRequirements{
   private Hashtable<String, Boolean> collection;
 
   private boolean hasElevator; //whether the library has an elevator
+  private ArrayList<String> bookList; 
   
   /**
    * Constructor
@@ -21,20 +22,6 @@ public class Library extends Building implements LibraryRequirements{
     this.collection = new Hashtable<>();
   }
   
-  /**
-   * overloaded Constructor version 1
-   * @param name the name of the library
-   * @param address the location of the library
-   * @param nFloors the number of floors in the library
-   * @param hasE whether the library has an elevator
-   */
-  public Library(String name, String address, int nFloors, boolean hasE) {
-    super(name,address,nFloors);
-    System.out.println("You have built a library: 📖");
-    this.collection = new Hashtable<>();
-    this.hasElevator = hasE;
-  }
-
   /**
    * overloaded Constructor version 1
    * @param name the name of the library
@@ -64,7 +51,7 @@ public class Library extends Building implements LibraryRequirements{
    */
   public String removeTitle(String title) {
     if (!collection.containsKey(title)) {
-      throw new RuntimeException (title + "is not in the collection of" + this.name + "library.");
+      throw new RuntimeException (title + " is not in the collection of " + this.name + " library.");
     } this.collection.remove(title,false);
     return title;
   }
@@ -75,7 +62,7 @@ public class Library extends Building implements LibraryRequirements{
    */
   public void checkOut(String title) {
     if (!this.collection.containsKey(title) || this.collection.get(title) == false) {
-      throw new RuntimeException (title + "is not available in" + this.name + "library.");
+      throw new RuntimeException (title + " is not available in " + this.name + " library.");
     } removeTitle(title);
   }
   
@@ -86,7 +73,7 @@ public class Library extends Building implements LibraryRequirements{
    */
   public void returnBook(String title) {
     if (!this.collection.containsKey(title) && this.collection.get(title) == true) {
-      throw new RuntimeException (title + "is already returned into" + this.name + "library.");
+      throw new RuntimeException (title + " is already returned into " + this.name + " library.");
     } addTitle(title);
   }
 
@@ -105,7 +92,24 @@ public class Library extends Building implements LibraryRequirements{
    * @return T/F
    */
   public boolean isAvailable(String title) {
-    return this.collection.get(title);
+    if (!this.containsTitle(title)) {
+      throw new RuntimeException(title + " is not in the collection list of " + this.name);
+    } return this.collection.get(title);
+  }
+
+  /**
+   * overloaded method for isAvailable to check if the list of books are available in the library
+   * @param booList a list of book titles
+   * @return T/F
+   */
+  public boolean isAvailable(ArrayList<String> bookList) {
+    for (String str: bookList) {
+      if (!this.containsTitle(str)) {
+        throw new RuntimeException(str + " is not in the collection list of " + this.name);
+      } else {
+        return this.collection.get(str);
+      } 
+    } return false;
   }
 
   /**
@@ -135,5 +139,11 @@ public class Library extends Building implements LibraryRequirements{
     neilson.showOptions();
     neilson.enter();
     neilson.goToFloor(4);
+    neilson.addTitle("Educated");
+    neilson.addTitle("Atomic Habits");
+    ArrayList<String> bookList = new ArrayList<> ();
+    bookList.add("Educated");
+    bookList.add("Atomic Habits");
+    neilson.isAvailable(bookList);
     }
   }
